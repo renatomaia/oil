beff2d96a61e311f9b30fe9b0efab201c4e0a0f5
Luiz Gustavo Nogara <nogara@tecgraf.puc-rio.br>
2006-06-08 22:21:17 +0000

    Fixed architecture definition.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/oil/branches/compbased@21358 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/oil/arch/comm.lua
+++ lua/oil/arch/comm.lua
@@ -1,44 +1,45 @@
 local component = require "loop.component.base"
+local port      = require "loop.component.base"
 
 module "oil.arch.comm"
 
 ProtocolType = component.Type{
-	protocol = component.Facet,
+	protocol = port.Facet,
 		-- operations :
 		--  call(reference, operation, ...)
 		--  handle(reference, operation, ...)
 	
-	codec = component.Receptacle,
+	codec = port.Receptacle,
 		-- optional receptacles
-	channelFactory = component.Receptacle,
-	error_handling = component.Receptacle  
+	channelFactory = port.Receptacle,
+	error_handling = port.Receptacle  
 }
 
-CORBAProtocolType = component.Type{ ProtocolType,
-	iop = component.Receptacle,
+CORBAProtocolType = component.Type({
+	iop = port.Receptacle,
 	
-	protocolHelper = component.Facet, -- TODO: change this name! 
+	protocolHelper = port.Facet, -- TODO: change this name! 
 		-- unmarshallHeader( stream ) 
-}
+}, ProtocolType)
 
 IOPType = component.Type{
-	iop = component.Facet, 
+	iop = port.Facet, 
 		-- operations
 		--  connect(reference)
 		--  listen(network_endpoint)
-	channelFactory = component.Receptacle,
-	protocolHelper = component.Receptacle, 
+	channelFactory = port.Receptacle,
+	protocolHelper = port.Receptacle, 
 }
 
 CodecType = component.Type{
-	codec = component.Facet,
+	codec = port.Facet,
 		-- operations :
 		--  newEncoder()
 		--  newDecoder()
 }
 
 ChannelFactoryType = component.Type{
-	factory = component.Facet,
+	factory = port.Facet,
 		-- operations :
 		--  connect()
 		--  send()
@@ -47,76 +48,76 @@ ChannelFactoryType = component.Type{
 }
 
 ConnResolverType = component.Type{
-	connector = component.Facet,
+	connector = port.Facet,
 		-- operations :
 		--  call( profile, operation, args)
-	protocols = component.HashReceptacle,
+	protocols = port.HashReceptacle,
 --  codecs    = "context receptacle",
 --  connmanager   = "receptacle",
-	references = component.HashReceptacle, -- to get the references to the objects / open profiles etc
+	references = port.HashReceptacle, -- to get the references to the objects / open profiles etc
 }
 
 SchedulerType = component.Type{
-	control = component.Facet,
-	threads = component.Facet,
-	sockets = component.Facet,
+	control = port.Facet,
+	threads = port.Facet,
+	sockets = port.Facet,
 }
 
 ResolverType = component.Type{
-	codec = component.Facet,
+	codec = port.Facet,
 }
 
 --- Reference
 ReferenceHandlerType = component.Type{
-		reference = component.Facet,
-		reference_resolver = component.HashReceptacle,
-		profile_resolver   = component.HashReceptacle,
+		reference = port.Facet,
+		reference_resolver = port.HashReceptacle,
+		profile_resolver   = port.HashReceptacle,
 }
 
 ReferenceResolverType = component.Type{
-		resolver = component.Facet,
-		codec = component.Receptacle,
+		resolver = port.Facet,
+		codec = port.Receptacle,
 		-- optional receptacles
-		profile_resolver = component.Receptacle,
+		profile_resolver = port.Receptacle,
 }
 
 ProfileResolverType = component.Type{
-		resolver = component.Facet, 
-		codec = component.Receptacle,
+		resolver = port.Facet, 
+		codec = port.Receptacle,
 }
 
 --- Invocation
 ProxyType = component.Type{
-		proxy = component.Facet,
-		protocol = component.Receptacle,
-		reference_handler = component.Receptacle
+		proxy = port.Facet,
+		protocol = port.Receptacle,
+		reference_handler = port.Receptacle
 }
 
 --- Object
 AccessPointType = component.Type{
-	point = component.Facet,
+	point = port.Facet,
 		-- operations :
 		--  listen(args)
-	protocol = component.HashReceptacle, 
+	protocol = port.HashReceptacle, 
 }
 
 DispatcherType = component.Type{
-		acceptor = component.Facet,
-		point    = component.Receptacle,
-		protocol = component.Receptacle,
-		reference_handler = component.Receptacle,
+		acceptor = port.Facet,
+		point    = port.Receptacle,
+		protocol = port.Receptacle,
+		reference_handler = port.Receptacle,
 }
 
 --- Interface
 
 ManagerType = component.Type{
-	manager = component.Facet,
-	proxy = component.Receptacle,
-	ir    = component.Receptacle,
+	manager = port.Facet,
+	proxy = port.Receptacle,
+	ir    = port.Receptacle,
 }
 InterfaceRepositoryType = component.Type{
-	ir = component.Facet,
-	proxy = component.Receptacle,
+	ir = port.Facet,
+	proxy = port.Receptacle,
 }
 
 -------------------------------------
