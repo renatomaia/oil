a15c4d4537f6636e4b92cb76fae9044f0f609154
Luiz Gustavo Nogara <nogara@tecgraf.puc-rio.br>
2006-07-07 12:31:47 +0000

    Modifications to the creation of proxies.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/oil/branches/compbased@21746 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/oil/ClientBroker.lua
+++ lua/oil/ClientBroker.lua
@@ -1,12 +1,14 @@
-
+local print   = print
+local require = require
 local oo      = require "oil.oo"
 
-module ("oil.ClientBroker", oo.class)
+module ("oil.ClientBroker", oo.class)                                       --[[VERBOSE]] local verbose = require "oil.verbose" 
+
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-function newproxy(self, ref, intfaceName)                                   --[[VERBOSE]] verbose:proxy("new proxy class for ", interface.repID)
+function newproxy(self, ref, intfaceName)                                   --[[VERBOSE]] verbose:proxy("new proxy class for ", intfaceName)
 	local decodedReference = self.reference:resolve(ref)
 	local proxy = self.factory:create(decodedReference, self.protocol, intfaceName)
 	return proxy 
