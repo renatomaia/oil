4694ff8116fdf0edc449b6cf7d4e22811b76f46e
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2007-07-06 22:55:42 +0000

    Define GIOP exception as the default exception.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/oil/branches/compbased@65159 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/oil/corba/giop/Exception.lua
+++ lua/oil/corba/giop/Exception.lua
@@ -1,5 +1,6 @@
 local oo        = require "oil.oo"
 local Exception = require "oil.Exception"
+local assert    = require "oil.assert"
 local giop      = require "oil.corba.giop"                                      --[[VERBOSE]] local verbose = require "oil.verbose"
 
 module("oil.corba.giop.Exception", oo.class)
@@ -7,9 +8,12 @@ module("oil.corba.giop.Exception", oo.class)
 __concat   = Exception.__concat
 __tostring = Exception.__tostring
 
+minor_code_value = 0
+
 function __init(_, except, ...)
 	local name = except[1]
 	except[1] = giop.SystemExceptionIDs[name] or name
 	return Exception.__init(_, except, ...)
 end
 
+assert.Exception = _M -- use GIOP exception as the default exception
