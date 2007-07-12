d86e90fbfba035ead89c1ab3143593b0b60faa10
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2007-07-12 17:55:27 +0000

    Architecture levels may not define a builder because they may only define a assemble function that changes component connections.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/oil/branches/compbased@65368 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/oil/builder/init.lua
+++ lua/oil/builder/init.lua
@@ -21,12 +21,15 @@ local FactoryFrm = "oil.builder.%s"
 local ArchFrm    = "oil.arch.%s"
 function build(customization, built)
 	for name in customization:gmatch(NamePat) do                                  --[[VERBOSE]] verbose:built(true, "creating ",name," components")
-		built = require(FactoryFrm:format(name)).create(built)                      --[[VERBOSE]] verbose:built(false)
+		local success, builder = pcall(require, FactoryFrm:format(name))
+		if success then
+			built = builder.create(built)                                             --[[VERBOSE]] else verbose:built("unable to load builder for architecture ",name)
+		end                                                                         --[[VERBOSE]] verbose:built(false)
 	end
 	for name in customization:gmatch(NamePat) do                                  --[[VERBOSE]] verbose:built(true, "assembling ",name," components")
-		local success, architecture = pcall(require, ArchFrm:format(name))
+		local success, arch = pcall(require, ArchFrm:format(name))
 		if success then
-			architecture.assemble(built)                                              --[[VERBOSE]] else verbose:built("unable to load architecture definition for ",name)
+			arch.assemble(built)                                                      --[[VERBOSE]] else verbose:built("unable to load architecture definition for ",name)
 		end                                                                         --[[VERBOSE]] verbose:built(false)
 	end
 	return built
