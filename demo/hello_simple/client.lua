ca8e5eed9022d8df90bd9cc04bf738ef749de211
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2007-09-04 12:38:58 +0000

    Update of the demo to use the Ludo protocol.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/oil/branches/compbased@67052 ae0415b3-e90b-0410-900d-d0be9363c56b

--- demo/hello_simple/client.lua
+++ demo/hello_simple/client.lua
@@ -1,31 +1,11 @@
-require "oil"                           
+OIL_FLAVOR = "ludo;base"
 
-oil.Config.flavor = "DummySimple"
-oil.verbose:level(5)
+require "oil"                                             -- Load OiL package
 
---------------------------------------------------------------------------------
--- Get object reference from file ----------------------------------------------
+oil.main(function()
+	hello = oil.newproxy(assert(oil.readfrom("ref.ludo"))) -- Get proxy to object
 
-local ior
-local file = io.open("hello.ior")
-if file then
-	ior = file:read("*a")
-	file:close()
-else
-	print "unable to read IOR from file 'hello.ior'"
-	os.exit(1)
-end
-
---------------------------------------------------------------------------------
--- Create an object proxy for the supplied interface ---------------------------
-
-local hello = oil.newproxy(ior, "Hello")
-
---------------------------------------------------------------------------------
--- Access remote object --------------------------------------------------
-
-for i=1,3 do
-	print(hello:say_hello_to("world ".. i))
-end
-
-print(hello:say_2_strings("test1", "test2"))
+	hello:_set_quiet(false)                                 -- Access the object
+	for i = 1, 3 do print(hello:say_hello_to("world")) end
+	print("Object already said hello "..hello:_get_count().." times till now.")
+end)
