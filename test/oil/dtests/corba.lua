f609e692473ec120f8170c60b5aaf7c421a0f2c9
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2007-11-06 13:53:15 +0000

    New parameters to create proxies without waiting for the referred object existency and also do not performe interface narrowing on the object.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/oil/trunk@69870 ae0415b3-e90b-0410-900d-d0be9363c56b

--- test/oil/dtests/corba.lua
+++ test/oil/dtests/corba.lua
@@ -8,14 +8,15 @@ module "oil.dtests"
 timeout = 10
 querytime = .5
 
-local CORBALoc = "corbaloc::%s:%d/%s"
-function getbyinfo(proc, port, objkey)
+Reference = "corbaloc::%s:%d/%s"
+function resolve(proc, port, objkey, nowait, nonarrow)
 	local proxy = oil.newproxy(
-		CORBALoc:format(hosts[proc], port, objkey),
+		Reference:format(hosts[proc] or proc, port, objkey),
 		oil.corba.idl.object)
+	if nowait then return nonarrow and proxy or oil.narrow(proxy) end
 	for i = 1, timeout/querytime do
 		if not proxy:_non_existent() then
-			return oil.narrow(proxy)
+			return nonarrow and proxy or oil.narrow(proxy)
 		end
 		oil.sleep(querytime)
 	end
--- test/oil/dtests/ludo.lua
+++ test/oil/dtests/ludo.lua
@@ -9,16 +9,14 @@ module "oil.dtests"
 timeout = 10
 querytime = .5
 
-local LuDORef = "%s@%s:%d"
-function getbyinfo(proc, port, objkey)
-	local proxy = oil.newproxy(LuDORef:format(objkey, hosts[proc], port))
+Reference = "%s@%s:%d"
+function resolve(proc, port, objkey, nowait)
+	local proxy = oil.newproxy(Reference:format(objkey, hosts[proc] or proc, port))
+	if nowait then return proxy end
 	for i = 1, timeout/querytime do
 		local success, errmsg = oil.pcall(proxy._non_existent, proxy)
-		if
-			not success and
-			type(errmsg) == "table" and
-			errmsg.reason == "noimplement"
-		then -- op '_non_existent' is not provided by such object exists ;-)
+		if success or (type(errmsg) == "table" and errmsg.reason == "noimplement")
+		then -- '_non_existent' may not provided by such object exists ;-)
 			return proxy
 		end
 		oil.sleep(querytime)
