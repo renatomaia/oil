7e8ab7e5b561462659a31496889529b3bdb46c82
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2010-07-19 14:00:38 +0000

    Shorter way to display mashaled streams
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/oil/branches/OIL_0_5@108098 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/oil/verbose.lua
+++ lua/oil/verbose.lua
@@ -62,45 +62,63 @@ _M:newlevel{ "transport" }
 _M:newlevel{ "hexastream" }
 _M:newlevel{ "idltypes" }
 
-function _M:hexastream(codec, cursor)
+function _M:hexastream(codec, cursor, prefix)
 	if self.flags.hexastream then
 		local stream = codec:getdata()
-		local base = codec.previousend
-		local viewer = self.viewer
-		local output = viewer.output
-		local lines = string.format("%%0%dx:", math.ceil(math.log10((base+#stream)/16)+1))
-		local count = 0
-		local text = {}
-		for char in stream:gmatch("(.)") do
-			column = math.mod(count, 16)
-			if column == 0 then
-				output:write(lines:format(base+count))
-			end
-			local hexa
-			if cursor[count+1]
-				then hexa = "[%02x]"
-				else hexa = " %02x "
-			end
-			output:write(hexa:format(string.byte(char)))
-			if char == "\0" then
-				text[#text+1] = "?"
-			elseif char:match("[%w%p ]") then
-				text[#text+1] = char
-			else
-				text[#text+1] = "."
-			end
-			count = count + 1
-			if count == #stream then
-				output:write(string.rep("    ", 15-column))
-				text[#text+1] = string.rep(" ", 15-column)
-				if column < 8 then output:write("  ") end
-				column = 15
-			end
-			if column == 15 then
-				output:write("  |"..table.concat(text).."|\n")
-				text = {}
-			elseif column == 7 then
-				output:write("  ")
+		if prefix then stream = string.rep("\0", prefix)..stream end
+		local last = #stream
+		for count = 1, last do
+			if cursor[count] then
+				for count = last, count, -1 do
+					if cursor[count] == false then
+						last = math.min(last, 16*math.ceil(count/16))
+						break
+					end
+				end
+				local base = codec.previousend
+				local output = self.viewer.output
+				local lines = string.format("%%0%dx:", math.ceil(math.log10((base+last)/16))+1)
+				local text = {}
+				for count = count-(count-1)%16, last do
+					column = math.mod(count-1, 16)
+					-- write line start if necessary
+					if column == 0 then
+						output:write(lines:format(base+count-1))
+					end
+					-- write hexadecimal code
+					local hexa
+					if cursor[count]
+						then hexa = "[%02x]"
+						else hexa = " %02x "
+					end
+					local code = stream:byte(count, count)
+					output:write(hexa:format(code))
+					if code == 0 then
+						text[#text+1] = "."
+					elseif code == 255 then
+						text[#text+1] = "#"
+					elseif stream:match("^[%w%p ]", count) then
+						text[#text+1] = stream:sub(count, count)
+					else
+						text[#text+1] = "?"
+					end
+					-- write blank if reached the end of the stream
+					if count == last or cursor[count+1] == "end" then
+						output:write(string.rep("    ", 15-column))
+						text[#text+1] = string.rep(" ", 15-column)
+						if column < 8 then output:write("  ") end
+						column = 15
+					end
+					-- write ASCII text if last column, or a blank space if middle column
+					if column == 15 then
+						output:write("  |"..table.concat(text).."|\n")
+						text = {}
+					elseif column == 7 then
+						output:write("  ")
+					end
+					if cursor[count+1] == "end" then break end
+				end
+				break
 			end
 		end
 	end
