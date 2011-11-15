f3f1e3f34cebe7dd13da14262b892995731701be
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2011-11-15 13:24:59 +0000

    Bugfix to catch LuDO mashaling errors.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/oil/trunk@124057 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/oil/ludo/Channel.lua
+++ lua/oil/ludo/Channel.lua
@@ -5,6 +5,8 @@
 
 
 local _G = require "_G"                                                         --[[VERBOSE]] local verbose = require "oil.verbose"
+local error = _G.error
+local pcall = _G.pcall
 local tonumber = _G.tonumber
 
 local oo = require "oil.oo"
@@ -17,12 +19,78 @@ local Channel = require "oil.protocol.Channel"
 
 local LuDOChannel = class({}, Channel)
 
-local MessageFmt = "%d\n%s"
-function LuDOChannel:sendvalues(...)                                            --[[VERBOSE]] verbose:message("sending values ",verbose.viewer:tostring(...))
+local function encodemsg(self, ...)                                             --[[VERBOSE]] verbose:message("sending values ",verbose.viewer:tostring(...))
 	local encoder = self.context.codec:encoder()
 	encoder:put(...)
 	local data = encoder:__tostring()
-	return self:send(MessageFmt:format(#data, data))
+	return #data.."\n"..data
+end
+
+local function decodemsg(self, bytes)
+	return self.context.codec:decoder(bytes):get()
+end
+
+local function receivevalues(self, timeout)
+	local size = self.pendingsize
+	if size then                                                                  --[[VERBOSE]] verbose:message("reading a previous incomplete message (got only its size)")
+		self.pendingsize = nil
+	else
+		local bytes, except = self:receive(nil, timeout)
+		if bytes then
+			size = tonumber(bytes)
+			if size == nil then                                                       --[[VERBOSE]] verbose:message("LuDO failure: illegal message size")
+				return nil, Exception{
+					"invalid LuDO message size (got $size)",
+					error = "badmessage",
+					size = bytes,
+				}
+			end
+		end
+	end
+	local bytes, except = self:receive(size, timeout)
+	if bytes ~= nil then
+		return self.context.codec:decoder(bytes):get()
+	elseif except.error == "timeout" then                                         --[[VERBOSE]] verbose:message("message was not available before timeout")
+		self.pendingsize = size
+	end
+	return error(except)
+end
+
+local function doreply(self, ok, requestid, success, ...)
+	if not ok then return nil, requestid end
+	local request, except = self[requestid]
+	if request then
+		self[requestid] = nil
+		request.channel = nil
+		request:setreply(success, ...)
+		self:signal("read", request)
+	else                                                                          --[[VERBOSE]] verbose:message("LuDO failure: reply for unknown request ID")
+		except = Exception{
+			"unexpected LuDO reply ID (got $requestid)",
+			error = "badmessage",
+			requestid = requestid,
+		}
+	end
+	return request, except
+end
+
+
+
+function LuDOChannel:sendvalues(request_id, ...)                                            --[[VERBOSE]] verbose:message("sending values ",verbose.viewer:tostring(...))
+	local ok, result = pcall(encodemsg, self, request_id, ...)
+	if ok then
+		self:trylock("write")
+		ok, result = self:send(result)
+		self:freelock("write")
+	else                                                                          --[[VERBOSE]] verbose:message("message encoding failed")
+		local ok, result = pcall(encodemsg, self, request_id, false, result)
+		if ok then
+			self:trylock("write")
+			self:send(result)
+			self:freelock("write")
+		end
+	end
+	return ok, result
 end
 
 function LuDOChannel:receivevalues(timeout)
@@ -44,30 +112,13 @@ function LuDOChannel:receivevalues(timeout)
 	end
 	local bytes, except = self:receive(size, timeout)
 	if bytes ~= nil then
-		return true, self.context.codec:decoder(bytes):get()
+		return pcall(decodemsg, self, bytes)
 	elseif except.error == "timeout" then                                         --[[VERBOSE]] verbose:message("message was not available before timeout")
 		self.pendingsize = size
 	end
 	return nil, except
 end
 
-local function doreply(self, ok, requestid, success, ...)
-	if not ok then return nil, requestid end
-	local request, except = self[requestid]
-	if request then
-		self[requestid] = nil
-		request.channel = nil
-		request:setreply(success, ...)
-		self:signal("read", request)
-	else                                                                          --[[VERBOSE]] verbose:message("LuDO failure: reply for unknown request ID")
-		except = Exception{
-			"unexpected LuDO reply ID (got $requestid)",
-			error = "badmessage",
-			requestid = requestid,
-		}
-	end
-	return request, except
-end
 function LuDOChannel:processmessage(timeout)
 	return doreply(self, self:receivevalues(timeout))
 end
